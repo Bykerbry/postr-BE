fbb94b095791f00b0a1d4c0e48c2d7eab8a751af
Bryce <bykerbry@umich.edu>
2020-04-28 22:29:58 -0400

    cors added

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const userRouter = require('./routes/users')
 const postRouter = require('./routes/posts')
 const commentRouter = require('./routes/comments')
 require('./db/mongoose')
 const port = process.env.PORT || 8080
 
+app.use(cors())
 app.use(express.json())
 app.use(userRouter)
 app.use(postRouter)
